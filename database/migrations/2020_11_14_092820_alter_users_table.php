e6e4148d783e7fb6292a1efe5984585002e52ac9
Ahmad J. Joda <joda.ahmad.96@gmail.com>
2021-05-29 16:00:20 +0300

    First commit

---
+++ database/migrations/2020_11_14_092820_alter_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')
+                ->nullable()
+                ->unique()
+                ->after('email_verified_at');
+
+            $table->enum('type', ['user', 'admin', 'super-admin'])
+                ->after('profile_photo_path')
+                ->default('user');
+
+            $table->string('phone')->nullable()->after('type')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('current_team_id');
+            $table->dropColumn('profile_photo_path');
+            $table->dropColumn('type');
+            $table->dropColumn('phone');
+        });
+    }
+}
